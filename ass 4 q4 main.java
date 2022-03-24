95142315d56fda25212d6c8b76b26b97fa09223c
jayavarshni <101458029+jayavarshni@users.noreply.github.com>
2022-03-24 12:40:36 +0530

    assignment 4

---
+++ ass 4 q4 main.java
@@ -0,0 +1,12 @@
+package Question4;
+
+public class Main
+{
+	public static void main(String gg[])
+	{
+		PriorityQueueDemo pq=new PriorityQueueDemo();
+		pq.sort1();
+	}
+}
+© 2022 GitHub, Inc.
+Ter
